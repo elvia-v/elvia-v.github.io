0a3e08718acbc839e76bd6d20da5a0b36ef5ef28
vio1ia <99840202+elvia-v@users.noreply.github.com>
2023-09-01 23:38:52 -0700

    added border and deleted things

--- hand/scripts/scripts.js
+++
@@ -1,9 +0,0 @@
-
-function gay() {
-  var x = document.getElementById("myDIV","myDIV2","myDIV3");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
